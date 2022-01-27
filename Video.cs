f1fcdc7f3e921ffcee4b551df6be6ad33b651411
Alan Robson <alanr@live.com.au>
2022-01-28 10:28:49 +1100

    Add project files.

---
+++ Video.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Rename.cs" company="Software Inc.">
+//   A.Robson
+// </copyright>
+// <summary>
+//   Rename music and training files to a consistant format.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace PlayList
+{
+    using Microsoft.WindowsAPICodePack.Shell;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Video
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
